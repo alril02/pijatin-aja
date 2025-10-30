58813447d1da16291b1e034b0edbd36188eb0a65
arilaminudin28-star <arilaminudin28@gmail.com>
2025-10-30 14:44:00 +0800

    buat form login
    membenarkan mainlayoutnya
    menambahkan loginlayout
    menambahkan forgotpassword
    menambahkan loginpage
    menambahkan registerpage

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,25 @@
+import IndexPages from 'pages/IndexPages.vue'
+
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
-  },
+    children: [{ path: '', component: () => import('pages/IndexPages.vue')}],
+},
+
+{
+  path: '/login',
+  component: () => import('layouts/Loginlayout.vue'),
+  children: [{ path: '', component: () => import('pages/login/LoginPages.vue')}],
+},
+
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
+    path: '/:catchAll(.)',
     component: () => import('pages/ErrorNotFound.vue')
-  }
+  },
 ]
 
 export default routes
